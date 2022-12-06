a6542a5cdd233044c7bcfd1667e7851112dce0ac
Adam Hoar <42139484+amh4@users.noreply.github.com>
2022-12-06 15:50:50 +0000

    Completed test to count number of words in string

--- lib/diary.rb
+++ lib/diary.rb
@@ -2,3 +2,7 @@ def make_snippet(str)
   array = str.split(" ")
   return array[0,5].join(" ")
 end
+
+def count_words(str)
+  return str.split(" ").length
+end
--- spec/diary_spec.rb
+++ spec/diary_spec.rb
@@ -1,7 +1,12 @@
 require 'diary'
 
 describe "Returns string" do
-    it "Returns the first five words of string" do
+  it "Returns the first five words of string" do
     expect(make_snippet("First five words of string to return")).to eq "First five words of string"
   end
+
+  it "Returns the number of words in the string" do
+    expect(count_words("One Two Three")).to eq 3
+  end
+
 end
